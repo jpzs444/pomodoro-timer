30dfab5b7b5f430160ab42b3fe7a910794ce7c93
jpzs444 <75724780+jpzs444@users.noreply.github.com>
2024-02-16 17:18:54 +0800

    chore: Upload icons

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import tomato from './tomato.png';
+import play from './play.png';
+import pause from './pause.png';
+import reset from './reset.png';
+import arrow from './arrow.png';
+
+export {
+  tomato,
+  play,
+  pause,
+  reset, 
+  arrow,
+}
